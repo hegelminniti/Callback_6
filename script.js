ffbafdbb5f29c8db66d33344b16676f506f9b6aa
Hegsina <hegelminniti@gmail.com>
2023-11-28 11:10:27 +0100

    Callbacks - Exercise 6

---
+++ script.js
@@ -0,0 +1,18 @@
+// Dato l'oggetto { name: "John", age: 30 } crea una funzione che simula un'operazione asincrona, come il recupero dei dati da un'API. 
+// -Implementare una callback per gestire i dati recuperati. 
+// -Utilizzare setTimeout per simulare il ritardo dell'operazione.
+
+
+function fetchDataFromAPI(callback) {
+    setTimeout(function () {
+      const data = { name: "John", age: 30 };
+      callback(data);
+    }, 1000);
+  }
+  
+
+function handleData(data) {
+    console.log("Fetched data:", data);
+}
+
+fetchDataFromAPI(handleData);
